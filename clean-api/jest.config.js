fa3d3c0a1f670db46c6e5f460e5c609296c5f497
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-19 08:20:26 -0300

    chore: add jest

---
+++ clean-api/jest.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  roots: [
+    '<rootDir>'
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
