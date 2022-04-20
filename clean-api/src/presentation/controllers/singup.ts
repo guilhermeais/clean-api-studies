e23769d32691a06aef149d831451012e1adcb273
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-20 08:13:00 -0300

    refactor: remove old verifications of SignUpController

--- clean-api/src/presentation/controllers/singup.ts
+++ clean-api/src/presentation/controllers/singup.ts
@@ -4,14 +4,6 @@ import { Controller } from '../protocols/controller'
 import { HttpResponse, HttpRequest } from '../protocols/http'
 export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFields) {
