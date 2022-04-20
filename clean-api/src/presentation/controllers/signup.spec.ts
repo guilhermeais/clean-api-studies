dae221616bc3a9f2937332dc66288d2c2113d62c
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-19 21:23:13 -0300

    chore: add passWithNoTest in jest config

---
+++ clean-api/src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './singup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 name is not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
