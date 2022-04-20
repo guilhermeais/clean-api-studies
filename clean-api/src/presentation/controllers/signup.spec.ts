8574dcb55ae90f2cdc30e8931d9340adda80a25d
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-19 22:05:34 -0300

    test: ensure SingUpController returns 400 if no email is provided

--- clean-api/src/presentation/controllers/signup.spec.ts
+++ clean-api/src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from './singup'
 
 describe('SignUp Controller', () => {
-  test('Should return 400 name is not provided', () => {
+  test('Should return 400 if name is not provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -13,5 +13,19 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if email is not provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
