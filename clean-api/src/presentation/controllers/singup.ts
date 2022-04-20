852a1e9ca8188498c6d0e49a820e7182db9fe995
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-19 22:34:28 -0300

    feat: ensure SignUpController returns 400 if no password is provided

--- clean-api/src/presentation/controllers/singup.ts
+++ clean-api/src/presentation/controllers/singup.ts
@@ -11,7 +11,7 @@ export class SignUpController {
       return badRequest(new MissingParamError('email'))
     }
 
-    const requiredFields = ['name', 'email']
+    const requiredFields = ['name', 'email', 'password']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
