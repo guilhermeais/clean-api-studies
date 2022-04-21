c4011ae04bc92584f9b1de1790036316d71c0b92
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-20 21:13:22 -0300

    test: ensure SignUPController calls EmailValidator with correct email

--- clean-api/src/presentation/controllers/signup.spec.ts
+++ clean-api/src/presentation/controllers/signup.spec.ts
@@ -94,4 +94,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toStrictEqual(new InvalidParamError('email'))
   })
+
+  test('Should call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(emailValidatorStub.isValid).toHaveBeenCalledWith(httpRequest.body.email)
+  })
 })
