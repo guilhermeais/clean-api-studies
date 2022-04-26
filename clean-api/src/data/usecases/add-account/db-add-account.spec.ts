cbce845637944ff2be3bab15dc2e26e873dc5504
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-26 13:15:52 -0300

    test: ensure DbAddAccount throw if Encrypter throws

--- clean-api/src/data/usecases/add-account/db-add-account.spec.ts
+++ clean-api/src/data/usecases/add-account/db-add-account.spec.ts
@@ -37,4 +37,17 @@ describe('DbAddAccount Usecase', () => {
     await sut.add(accountData)
     expect(encrypterStub.encrypt).toHaveBeenCalledWith('valid_password')
   })
+
+  test('Should throw if Encrypter throws', async () => {
+    const { sut, encrypterStub } = makeSut()
+    jest.spyOn(encrypterStub, 'encrypt')
+      .mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error('some_error'))))
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const sutPromise = sut.add(accountData)
+    await expect(sutPromise).rejects.toThrow(new Error('some_error'))
+  })
 })
