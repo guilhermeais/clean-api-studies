8b0e6aaf741e1b82e05b8b84c1a1b5c148bbaa9b
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-04-26 21:11:51 -0300

    test: ensure DbAddAccount returns an account on success

--- clean-api/src/data/usecases/add-account/db-add-account.spec.ts
+++ clean-api/src/data/usecases/add-account/db-add-account.spec.ts
@@ -93,4 +93,21 @@ describe('DbAddAccount Usecase', () => {
     const sutPromise = sut.add(accountData)
     await expect(sutPromise).rejects.toThrow(new Error('some_error'))
   })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
