19a5a1f9fed2a95188c07c5ddb12c143216913b4
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-04-29 13:11:55 -0300

    test: ensure AccountMongoRepository returns an AccountModel on success

---
+++ clean-api/src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,29 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    const mongoURL = process.env.MONGO_URL ?? ''
+    await MongoHelper.connect(mongoURL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.diconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    const sut = new AccountMongoRepository()
+    const mockAccountInput = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+    const account = await sut.add(mockAccountInput)
+
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe(mockAccountInput.name)
+    expect(account.email).toBe(mockAccountInput.email)
+    expect(account.password).toBe(mockAccountInput.password)
+  })
+})
