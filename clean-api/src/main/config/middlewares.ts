131d2ea08df493926aaf544961e2edff05c15bf0
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-04-29 21:49:18 -0300

    feat: ensure requests return json as default content type

--- clean-api/src/main/config/middlewares.ts
+++ clean-api/src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/body-parser'
+import { contentType } from '../middlewares/content-type'
 import { cors } from '../middlewares/cors'
 
 export function setupMiddlewares (app: Express): void {
+  app.use(contentType)
   app.use(bodyParser)
   app.use(cors)
 }
