9275638a2edc479f3f95c8b10f3a5439bcfff231
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-04-29 23:57:36 -0300

    feat: add ExpressROuteAdapter

---
+++ clean-api/src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express'
+import { Controller, HttpRequest } from '../../presentation/protocols'
+
+export function adaptRoute (controller: Controller) {
+  return async function (req: Request, res: Response): Promise<void> {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+    try {
+      const httpResponse = await controller.handle(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    } catch (error) {
+      res.status(500).json({
+        message: error.message
+      })
+    }
+  }
+}
