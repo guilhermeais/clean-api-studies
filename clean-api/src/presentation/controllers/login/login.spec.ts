ba900390f5fa52171a2ee584c5ab21647f54f8dc
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-05-07 15:20:31 -0300

    refactor: remove code duplication

--- clean-api/src/presentation/controllers/login/login.spec.ts
+++ clean-api/src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,17 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
 import { badRequest } from '../../helpers/http-helper'
-import { EmailValidator } from '../signup/signup-protocols'
+import { EmailValidator, HttpRequest } from '../signup/signup-protocols'
 import LoginController from './login'
+
+function makeFakeRequest (): HttpRequest {
+  return {
+    body: {
+      email: 'invalid_email',
+      password: 'any_password'
+
+    }
+  }
+}
 
 function makeEmailValidator (): EmailValidator {
   class EmailValidatorStub implements EmailValidator {
@@ -30,8 +40,8 @@ describe('Login Controller', () => {
     const { sut } = makeSut()
     const httpRequest = {
       body: {
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
+        password: 'any_password'
+
       }
     }
     const httpResponse = await sut.handle(httpRequest)
@@ -52,13 +62,7 @@ describe('Login Controller', () => {
   test('Should call EmailValidator with correct email', async () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid')
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(emailValidatorStub.isValid).toHaveBeenCalledWith(httpRequest.body.email)
   })
@@ -66,13 +70,8 @@ describe('Login Controller', () => {
   test('Should return 400 if an invalid email is provided', async () => {
     const { sut, emailValidatorStub } = makeSut()
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
-    const httpRequest = {
-      body: {
-        email: 'invalid_email',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeRequest()
+
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
   })
