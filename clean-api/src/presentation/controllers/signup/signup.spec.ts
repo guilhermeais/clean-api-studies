3fa5757dd5601840aad92a52318ac48373e7f4f1
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-05-12 08:14:10 -0300

    test: esnure SignUpController returns 400 if Validataion fails

--- clean-api/src/presentation/controllers/signup/signup.spec.ts
+++ clean-api/src/presentation/controllers/signup/signup.spec.ts
@@ -220,4 +220,13 @@ describe('SignUp Controller', () => {
     await sut.handle(httpRequest)
     expect(validationStub.validate).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    const errorMock = new MissingParamError('any_field')
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(errorMock)
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(errorMock))
+  })
 })
