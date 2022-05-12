e343b47c21cd3b2bcd6b1c869205ebce2abb9308
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-05-12 08:51:29 -0300

    refactor: move required field validation to the ValidationComposite

--- clean-api/src/main/factories/signup.ts
+++ clean-api/src/main/factories/signup.ts
@@ -6,6 +6,7 @@ import { SignUpController } from '../../presentation/controllers/signup/singup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
+import { makeSignUpValidation } from './signup-validation'
 
 export function makeSignUpController (): Controller {
   const salt = 12
@@ -13,7 +14,7 @@ export function makeSignUpController (): Controller {
   const bcryptAdapter = new BcryptAdapter(salt)
   const addAccountRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, addAccountRepository)
-  const signupController = new SignUpController(emailValidator, dbAddAccount)
+  const signupController = new SignUpController(emailValidator, dbAddAccount, makeSignUpValidation())
   const logErrorRepository = new LogMongoRepository()
   return new LogControllerDecorator(signupController, logErrorRepository)
 }
