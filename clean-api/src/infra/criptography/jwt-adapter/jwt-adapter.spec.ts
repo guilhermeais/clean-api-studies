f79182441f78bdb8da1cbabf7f5692b9033974b3
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-05-19 20:54:03 -0300

    feat: ensure JwtAdapter return a token on success

--- clean-api/src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ clean-api/src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -1,10 +1,19 @@
 import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
+jest.mock('jsonwebtoken', () => ({
+  sign: async (): Promise<string> => await Promise.resolve('any_token')
+}))
 describe('JWT Adapter', () => {
   test('Should call sign with correct values', async () => {
     jest.spyOn(jwt, 'sign')
     const sut = new JwtAdapter('secret')
     await sut.encrypt('any_id')
     expect(jwt.sign).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should returns a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
