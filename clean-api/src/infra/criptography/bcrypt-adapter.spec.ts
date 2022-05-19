0bfb2a79b9673d704f40fb337fc459fc35630366
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-05-19 20:17:19 -0300

    feat: ensure BcryptAdapter returns true when compare succeeds

--- clean-api/src/infra/criptography/bcrypt-adapter.spec.ts
+++ clean-api/src/infra/criptography/bcrypt-adapter.spec.ts
@@ -48,4 +48,10 @@ describe('Bcrypt Adapter', () => {
     await sut.compare('any_value', 'any_hash')
     expect(bcrypt.compare).toHaveBeenCalledWith('any_value', 'any_hash')
   })
+
+  test('Should return true when compare succeeds', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(true)
+  })
 })
