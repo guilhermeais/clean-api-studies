580b6c7f23b5b95c6296c769a071eb3bd9de9215
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-06-09 08:33:45 -0300

    refactor: rename signup routes with login routes

---
+++ clean-api/src/main/routes/login-routes.test.ts
@@ -0,0 +1,28 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    const mongoURL = process.env.MONGO_URL ?? ''
+    await MongoHelper.connect(mongoURL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Guilherme Teixeira Ais',
+          email: 'guilherme.teixeira@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+})
