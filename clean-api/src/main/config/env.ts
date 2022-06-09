d0716d3383658c6d3f0706c91abd9ea0be357716
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-06-09 08:58:09 -0300

    feat: add LoginFactory

--- clean-api/src/main/config/env.ts
+++ clean-api/src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://0.0.0.0:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '7f2d633d-3662-44a7-b314-4bc74b81a6bf'
 }
