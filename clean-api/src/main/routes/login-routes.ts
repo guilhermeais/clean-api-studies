d8d7ad099fe61d371cf512d0d322331e758e1c49
Guilherme Teixeira <guilherme.teixeira@ctctech.com.br>
2022-06-09 09:04:47 -0300

    feat: add login route

--- clean-api/src/main/routes/login-routes.ts
+++ clean-api/src/main/routes/login-routes.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
+import { makeLoginController } from '../factories/login/login-factory'
 import { makeSignUpController } from '../factories/signup/signup-factory'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
+  router.post('/login', adaptRoute(makeLoginController()))
 }
