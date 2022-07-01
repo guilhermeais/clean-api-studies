115395c390a81a0becb6eb48985abc0819f57759
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-06-30 23:25:44 -0300

    test: ensure SurveyMongoRepository add a new survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,43 @@
+import { Collection } from 'mongodb'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+describe('Survey Mongo Repository', () => {
+  let surveyCollection: Collection
+  beforeAll(async () => {
+    const mongoURL = process.env.MONGO_URL ?? ''
+    await MongoHelper.connect(mongoURL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  function makeSut (): SurveyMongoRepository {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should create a survey on add success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        {
+          answer: 'any_answer',
+          image: 'any_image'
+        },
+        {
+          answer: 'other_answer'
+        }
+      ]
+    })
+
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
