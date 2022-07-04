ae45dc0f0b2e8b843c7e0e135edcdfb98c40cc82
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-04 13:31:18 -0300

    feat: add AddSurvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,19 @@
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+import { Validation } from '../../../../presentation/protocols/validation'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('AddSurveyValidation Factory', () => {
+  test('Should call ValidationComposite with all validators', () => {
+    makeAddSurveyValidation()
+    const validations: Validation [] = []
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith([
+      ...validations
+    ])
+  })
+})
