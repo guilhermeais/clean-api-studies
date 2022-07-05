ea57375f9156309a59a849f2a62dcc9e48cf785b
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-04 23:19:23 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,12 +2,46 @@ import { HttpRequest } from '../protocols'
 import { forbidden } from '../helpers/http/http-helper'
 import { AccessDeniedError } from '../errors'
 import { AuthMiddleware } from './auth-middleware'
+import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { AccountModel } from '../../domain/models/account'
+
+function makeFakeAccount (): AccountModel {
+  return {
+    id: 'some_id',
+    email: 'any_email@mail.com',
+    name: 'any_name',
+    password: 'any_hashed_password'
+  }
+}
 
 describe('Auth Middleware', () => {
   test('Should return 403 if no x-access-token exists in headers', async () => {
-    const sut = new AuthMiddleware()
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (accessToken: string): Promise<AccountModel> {
+        return await Promise.resolve(makeFakeAccount())
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
     const httpRequest: HttpRequest = {}
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+  test('Should call LoadAccountByToken with correct accessToken', async () => {
+    class LoadAccountByTokenStub implements LoadAccountByToken {
+      async load (accessToken: string): Promise<AccountModel> {
+        return await Promise.resolve(makeFakeAccount())
+      }
+    }
+    const loadAccountByTokenStub = new LoadAccountByTokenStub()
+    jest.spyOn(loadAccountByTokenStub, 'load')
+    const sut = new AuthMiddleware(loadAccountByTokenStub)
+    const httpRequest: HttpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+    await sut.handle(httpRequest)
+    expect(loadAccountByTokenStub.load).toHaveBeenCalledWith('any_token')
   })
 })
