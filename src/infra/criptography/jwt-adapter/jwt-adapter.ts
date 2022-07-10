40aeb33b7e4f0b7d8eac9eb610fc8f25825fd038
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-09 21:18:23 -0300

    feat: ensrue JwtAdapter returns a decrypted value on success

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -11,7 +11,7 @@ export class JwtAdapter implements Encrypter, Decrypter {
   }
 
   async decrypt (token: string): Promise<string> {
-    await jwt.verify(token, this.secret)
-    return null
+    const value = await jwt.verify(token, this.secret)
+    return value as string
   }
 }
