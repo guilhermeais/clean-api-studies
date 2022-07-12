cec56a79ff459ae52f327963327c386f2c0aa284
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-12 09:00:55 -0300

    test: ensure add survey route returns 204 if valid access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,9 +1,13 @@
+import { sign } from 'jsonwebtoken'
 import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+import env from '../config/env'
 
 let surveyCollection: Collection
+let accountCollection: Collection
+
 describe('Survey Routes', () => {
   beforeAll(async () => {
     const mongoURL = process.env.MONGO_URL ?? ''
@@ -17,6 +21,9 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.deleteMany({})
+
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
   })
 
   describe('POST /surveys', () => {
@@ -36,6 +43,45 @@ describe('Survey Routes', () => {
           ]
         })
         .expect(403)
+    })
+
+    test('Should return 204 on add survey with valid access token', async () => {
+      const result = await accountCollection.insertOne({
+        name: 'Guilherme Teixeira Ais',
+        email: 'guilherme.teixeira@gmail.com',
+        password: 'somePassword',
+        role: 'admin'
+      })
+      const id = result.insertedId
+      const accessToken = sign({ id: id.toString() }, env.jwtSecret)
+
+      await accountCollection.updateOne(
+        {
+          _id: id
+        },
+        {
+          $set: {
+            accessToken
+          }
+        }
+      )
+
+      await request(app)
+        .post('/api/surveys')
+        .set('x-access-token', accessToken)
+        .send({
+          question: 'Question',
+          answers: [
+            {
+              answer: 'Answer 1',
+              image: 'http://image-name.com'
+            },
+            {
+              answer: 'Answer 2'
+            }
+          ]
+        })
+        .expect(204)
     })
   })
 })
