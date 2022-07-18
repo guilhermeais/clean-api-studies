e3d28ec366c564effe8b015274dba09b9e621f0c
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-18 08:07:05 -0300

    test: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -45,4 +45,43 @@ describe('Survey Mongo Repository', () => {
       expect(survey).toBeTruthy()
     })
   })
+
+  describe('loadAll()', () => {
+    test('Should load all surveys on success', async () => {
+      const surveysMock = [
+        {
+          question: 'any_question',
+          answers: [
+            {
+              answer: 'any_answer',
+              image: 'any_image'
+            },
+            {
+              answer: 'any_answer'
+            }
+          ],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [
+            {
+              answer: 'other_answer',
+              image: 'other_image'
+            },
+            {
+              answer: 'other_answer'
+            }
+          ],
+          date: new Date()
+        }
+      ]
+      await surveyCollection.insertMany(surveysMock)
+      const sut = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(2)
+      expect(surveys[0].question).toBe(surveysMock[0].question)
+      expect(surveys[1].question).toBe(surveysMock[1].question)
+    })
+  })
 })
