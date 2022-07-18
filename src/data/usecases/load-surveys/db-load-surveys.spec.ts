8bb5d690130a5dd5303bcd6a8e82747f0d41f797
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-07-18 20:05:47 -0300

    fix: add mockdate

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,6 +1,7 @@
 import { SurveyModel } from '../../../domain/models/survey'
 import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
 import { DbLoadSurveys } from './db-load-surveys'
+import MockDate from 'mockdate'
 function makeFakeSurveys (): SurveyModel[] {
   return [
     {
@@ -51,6 +52,13 @@ function makeSut (): SutTypes {
   }
 }
 describe('DbLoadSurveys', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('Should call LoadSurveysRepository', async () => {
     const { sut, loadSurveysRepositoryStub } = makeSut()
     jest.spyOn(loadSurveysRepositoryStub, 'loadAll')
