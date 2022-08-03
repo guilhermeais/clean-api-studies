419a69488dc8228196355b91327b66d0839828f5
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-08-03 20:58:07 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'API desenvolvida no [curso de NodeJS](https://www.udemy.com/course/tdd-com-mango).',
+    version: '1.0.0'
+  }
+}
