1073500e1443a1615676d9e99dbae645d721d035
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-08-19 21:00:44 -0300

    refactor: remove unused code

--- src/infra/db/mongodb/helpers/query-builder.ts
+++ src/infra/db/mongodb/helpers/query-builder.ts
@@ -30,13 +30,6 @@ export class QueryBuilder {
     return this
   }
 
-  addFields (data: object): QueryBuilder {
-    this.query.push({
-      $addFields: data
-    })
-    return this
-  }
-
   project (data: object): QueryBuilder {
     this.query.push({
       $project: data
