77bec11743d4ec83b109664099bd705a4483db9b
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-08-19 21:26:34 -0300

    chore: exclude all files that contains '-protocols' from coverage

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '<rootDir>/src'
   ],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!**/test/**', '!**/protocols/**', '!**/models/**', '!**/usecases/**'],
+  collectCoverageFrom: ['<rootDir>/src/**/{!(*-protocols),}.ts', '!<rootDir>/src/main/**', '!**/test/**', '!**/protocols/**', '!**/models/**', '!**/usecases/**'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   transform: {
