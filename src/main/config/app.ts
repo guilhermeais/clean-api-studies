132a65743abbfc9ca8e8c138fe3a2e4fd777fe82
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-08-24 08:05:41 -0300

    feat: create function to setup static files routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import { setupSwagger } from './config-swagger'
 import { setupMiddlewares } from './middlewares'
 import { setupRoutes } from './routes'
+import { setupStaticFiles } from './static-files'
 
 const app = express()
+setupStaticFiles(app)
 setupSwagger(app)
 setupMiddlewares(app)
 setupRoutes(app)
